74e6d3426dd41f7828b6b38b1538e1fa205ac5c2
unknown <shzk47@163.com>
2018-12-29 13:52:35 +0800

    add timecomplex code file

---
+++ timecomplex.m
@@ -0,0 +1,17 @@
+n0 = 2:20;
+ 
+N1= power(2,n0);
+N2=n0;
+j=1;
+for i=n0
+    N2(j)=nchoosek(i,floor(i/2));
+   j=j+1;
+end
+ 
+plot(n0,N1,'-r+')
+%title('M=N/2')
+hold on
+xlabel('N value');
+ylabel('time complexity')
+plot(n0,N2,'-g*')
+legend('Williams''  method ','proposed method (M=N/2)',2)
