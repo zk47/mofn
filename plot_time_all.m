12900de4cfb3bb2b7f1b4c04f05a84f92d193212
zhao kang <5862784@qq.com>
2019-02-24 17:12:35 +0800

    修改了plot_time_all

--- plot_time_all.m
+++ plot_time_all.m
@@ -8,17 +8,22 @@ pv=0.8;
 delete(sprintf('mofn_struct_n%d.mat', n));
 [S2, Pd2, T2] = mofn_williams_pd2(pv, k, m, n);
 delete(sprintf('mofn_struct_n%d.mat', n));
+s1 = ones(1,k)*S1.t;
+p1=plot(1:k, s1, '--m');
+ts=sprintf('M=%d,N=%d',m,n);hold on;
+title(ts)
+
+s2 = ones(1,k)*S2.t;
+p2=plot(1:k, s2, '--b');hold on;
+
 T1=T1+S1.t;
 T2=T2+S2.t;
-%T1=[0.149,0.208,0.362,0.615,1.214,2.387,4.571,9.143,18.464,38.355,86.247];
-%T2=[0.94,0.159,0.187,0.286,0.406,0.495,0.670,0.876,1.113,1.441,1.695];
 
-plot(1:k,T1,'-r+')
-ts=sprintf('M=%d,N=%d',m,n);
-title(ts)
-hold on
-xlabel('number of scans');
-ylabel('Time consumed (s)')
-plot(1:k,T2,'-g*')
+p3=plot(1:k,T1,'-r+');hold on;
 
-legend('Williams CMMS + Williams CDP','proposed CMMS + Williams CDP',0)
+xlabel('scan number');
+ylabel('Time consumed (s)');
+p4=plot(1:k,T2,'-g*');
+
+legend([p1(1),p2(1),p3(1),p4(1)], 'Williams CMMS algorithm', 'proposed CMMS algorithm', 'Williams CMMS + Williams CDP', 'proposed CMMS + Williams CDP');
+%legend('Williams CMMS algorithm','proposed CMMS algorithm','Williams CMMS + Williams CDP','proposed CMMS + Williams CDP')
